a89c55d80d15835e00c3d97cf6ed22329eade642
blueskyy-ai <blue.skyy@softbank.ne.jp>
2026-09-20 20:53:38 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,14 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+    public static void main(String[] args) {
+        
+        Dictonary_Chapter21 dog = new Dictonary_Chapter21();
+        
+        String[] wordsToSearch = {"apple", "banana", "grape", "orange"};
+        
+        // 保存されると、ここの赤波線が消えます
+        dog.searchWords(wordsToSearch);
+    }
+}
+
